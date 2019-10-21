c27b9c35747c747f0e3e9c14e37655fc8d450221
imanakatsuki <imanakatsuki@mb>
2019-10-21 21:25:20 +0900

    付録１

--- javaTest/src/javaTest/Java.java
+++ javaTest/src/javaTest/Java.java
@@ -5,10 +5,10 @@ public class Java {
 	public static void main(String[] args) {
 		int a = new java.util.Scanner(System.in).nextInt();
 		int b = new java.util.Scanner(System.in).nextInt();
-		int c = a + b;
-		System.out.println(c);
-		int d = a - b;
-		System.out.println(d);
+		int c1 = a + b;
+		System.out.println(c1);
+		int c2 = a - b;
+		System.out.println(c2);
 
 	}
 

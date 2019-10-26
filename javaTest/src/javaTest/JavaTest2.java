051d9bc39926d3ce0354316b6c0832f281e00fcb
今中 月 <imanakatsuki@mb>
2019-10-26 20:46:39 +0900

    付録2

--- javaTest/src/javaTest/JavaTest2.java
+++ javaTest/src/javaTest/JavaTest2.java
@@ -3,8 +3,16 @@ package javaTest;
 public class JavaTest2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("年齢を入力");
+		int nenrei = new java.util.Scanner(System.in).nextInt();
+		if (nenrei == 0) {
+			int kakaku = 100;
+			System.out.println("価格は" + kakaku + "円");
+		} else {
+			int kakaku = 500;
+			System.out.println("年齢を入力");
+			System.out.println("価格は" + kakaku + "円");
+		}
 	}
 
 }

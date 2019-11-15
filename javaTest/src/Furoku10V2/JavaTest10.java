1074626f2e106cc378cb2be3fe2457ae1359808d
今中 月 <imanakatsuki@mb>
2019-11-15 20:50:52 +0900

    クラス変数クラス

--- javaTest/src/Furoku10V2/JavaTest10.java
+++ javaTest/src/Furoku10V2/JavaTest10.java
@@ -1,33 +1,32 @@
 package Furoku10V2;
+
 import java.util.Scanner;
+
 public class JavaTest10 {
 
 	public static void main(String[] args) {
-			System.out.println("モードを選択してください。（1or2):");
-			Scanner scanner = new Scanner(System.in);
-			int mode = scanner.nextInt();
-			System.out.println("生まれた年を西暦で入力して下さい");
-			Nensu.umare = scanner.nextInt();
+		System.out.println("モードを選択してください。（1or2):");
+		Scanner scanner = new Scanner(System.in);
+		int mode = scanner.nextInt();
+		System.out.println("生まれた年を西暦で入力して下さい");
+		Nensu.umare = scanner.nextInt();
 
-	    while(mode != 1) {
-	    	if (mode != 2) {
-	    	System.out.println("モードを選択してください。（1or2):");
-	    	 mode = scanner.nextInt();
-	    	} else {
-	    		break;
-	    	}
-	    }
-		if (mode == 1) {
-			Sub s = new Sub();
-			Sub.calcage();
+		while (mode != 1) {
+			if (mode != 2) {
+				System.out.println("モードを選択してください。（1or2):");
+				mode = scanner.nextInt();
+			} else {
+				break;
+			}
 		}
-		else {
-			Sub s = new Sub();
+		if (mode == 1) {
+			Sub s1 = new Sub();
+			Sub.calcage();
+		} else {
+			Sub s2 = new Sub();
 			Sub.calcyear();
 		}
 		scanner.close();
-		}
-
 	}
 
-
+}
